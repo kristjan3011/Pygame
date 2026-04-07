15df97cf8e2a48c8031dac57a70c6cd534187138
Kristjan Juhansoo <kristjan.juhansoo@vikk.ee>
2026-04-07 13:02:04 +0300

    Lisatud paar uut pilti ja tekst

--- Pygame'i asjad/Ülesanne2.py
+++ Pygame'i asjad/Ülesanne2.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import math
 
 pygame.init()
 
@@ -20,6 +21,15 @@ chat = pygame.transform.smoothscale(chat, (252, 200))#jutumull
 font = pygame.font.SysFont("arial", 28) # fondi valikud on: times_new_roman, calibri, comic_sans
 text = font.render("Tere, olen Kristjan", True, (255, 255, 255))
 
+#2.1 pildid
+logo = pygame.image.load("VIKK_logo.png")
+logo = pygame.transform.smoothscale(logo, (280, 70))#logo
+
+mook = pygame.image.load("mõõk.png")
+mook = pygame.transform.smoothscale(mook, (120, 160))#mõõk
+
+tort = pygame.image.load("tort.png")
+tort = pygame.transform.smoothscale(tort, (120, 120))
 #põhiloop
 while True:
     for event in pygame.event.get():
@@ -37,6 +47,35 @@ while True:
     screen.blit(chat, (250, 65))
 
     #tekst jutumulli sisse
+
     screen.blit(text, (280, 145))
 
+    #logo
+    screen.blit(logo, (25, 10))
+
+    #kaare tekst
+    font3 = pygame.font.SysFont("arial", 22, bold=True)
+    tekst_kaar = "0502 KIVELUT" #0502 KIVELUT, TULEVIK 2050
+
+    # kaare seadistus
+    center_x, center_y = 120, 5  # kaare keskpunkt
+    radius = 100  # kaare raadius
+    algus_nurk = 0.8  # kust kaar algab (radiaanides)
+
+    for i, tahl in enumerate(tekst_kaar):
+        nurk = algus_nurk + i * 0.13  # tähtede vahekaugus
+        x = center_x + radius * math.cos(nurk)
+        y = center_y + radius * math.sin(nurk)
+
+        tahl_surf = font3.render(tahl, True, (0, 150, 200))  #sinakas värvus
+        tahl_surf = pygame.transform.rotate(tahl_surf, -math.degrees(nurk) + 90)
+
+        rect = tahl_surf.get_rect(center=(x, y))
+        screen.blit(tahl_surf, rect)
+
+    #mõõk
+    screen.blit(mook, (510, 140))
+
+    #tort
+    screen.blit(tort, (400, 200))
     pygame.display.flip()
